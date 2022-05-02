f069cdbe05bef8d137bb1c0930f3dca3c2a10b57
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-05-02 03:22:16 +0200

    input data section

--- main.py
+++ main.py
@@ -30,12 +30,78 @@ def about(root):
 
 
 def hozizontalSeparator(root):
-    ttk.Separator(root, orient='horizontal').pack(fill='x')
+    ttk.Separator(root, orient='horizontal').pack(fill=tk.X)
+
+
+def verticalSeparator(root):
+    ttk.Separator(root, orient='vertical').pack(side=tk.LEFT, fill=tk.Y)
+
+
+def inputData(frame):
+    frampady = 25
+    padding = 20
+    entryW = 30
+    entryH = 20
+    lblW = 12
+    lblFontSize = 16
+    titleFrame = tk.Frame(frame)
+    titleFrame.pack(pady=10)
+
+    titleGraph = tk.Label(titleFrame, text="Input data", font=(fontFamily, 20, "bold"), fg="green")
+    titleGraph.pack(fill=tk.X, pady=10)
+
+    lblFrame = tk.Frame(frame)
+    lblFrame.pack(pady=frampady)
+    funlbl = tk.Label(lblFrame, text="Function : ", font=(fontFamily, lblFontSize), fg="black", anchor='w', width=lblW)
+    funlbl.pack(side=tk.LEFT, padx=padding)
+    funEnry = tk.Entry(lblFrame, width=entryW, font=(fontFamily, lblFontSize))
+    funEnry.pack(side=tk.RIGHT, padx=padding)
+
+    maxFrame = tk.Frame(frame)
+    maxFrame.pack(pady=frampady)
+    maxXlbl = tk.Label(maxFrame, text="Max value of X : ", font=(fontFamily, lblFontSize), fg="black", anchor='w',
+                       width=lblW)
+    maxXlbl.pack(side=tk.LEFT, padx=padding)
+    maxXEnry = tk.Entry(maxFrame, width=entryW, font=(fontFamily, lblFontSize))
+    maxXEnry.pack(side=tk.RIGHT, padx=padding)
+
+    minFrame = tk.Frame(frame)
+    minFrame.pack(pady=frampady)
+    minXlbl = tk.Label(minFrame, text="Min value of X : ", font=(fontFamily, lblFontSize), fg="black", anchor='w',
+                       width=lblW)
+    minXlbl.pack(side=tk.LEFT, padx=padding)
+    minXEnry = tk.Entry(minFrame, width=entryW, font=(fontFamily, lblFontSize))
+    minXEnry.pack(side=tk.RIGHT, padx=padding)
+
+    submitFrame = tk.Frame(frame)
+    submitFrame.pack(pady=frampady)
+    btn = tk.Button(submitFrame, text="Plot", command=helloCallBack,width = 10,height = 20,font=(fontFamily, lblFontSize+2),background = "green",fg="white")
+    btn.pack()
+
+def helloCallBack():
+    print("Hello world")
+def plotGraph(frame):
+    titleFrame = tk.Frame(frame)
+    titleFrame.pack(pady=20)
+
+    titleGraph = tk.Label(titleFrame, text="Graph", font=(fontFamily, 20, "bold"), fg="green")
+    titleGraph.pack(fill=tk.X, pady=10)
 
 
 def body(root):
-    bodyFrame = tk.Frame(root, background="red")
+    bodyFrame = tk.Frame(root)
     bodyFrame.pack()
+    # separate the body frames into two frames
+    inputDataFrame = tk.Frame(bodyFrame, width=400, height=400)
+    inputDataFrame.pack(side=tk.LEFT, fill=tk.X)
+    inputDataFrame.pack_propagate(0)
+    verticalSeparator(bodyFrame)
+    graphFrame = tk.Frame(bodyFrame, width=400, height=400)
+    graphFrame.pack(side=tk.RIGHT)
+    graphFrame.pack_propagate(0)
+
+    inputData(inputDataFrame)
+    plotGraph(graphFrame)
 
 
 # Press the green button in the gutter to run the script.
